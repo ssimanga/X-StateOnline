a7ffee5c2f09f908042adbefa7783470f47a4bef
Simanga Shabalala <simangae@gmail.com>
2022-08-30 08:47:15 +0200

    Added Product Model and Product Repository

---
+++ X-StateOnline.DataAcess.Inmemory/ProductRepository.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using X_StateOnline.Core;
+using System.Runtime.Caching;
+using X_StateOnline.Core.Models;
+
+namespace X_StateOnline.DataAcess.Inmemory
+{
+   public class ProductRepository
+    {
+        ObjectCache cache =  MemoryCache.Default;
+        List<Product> products = new List<Product>();
+        public ProductRepository()
+        {
+            products = cache["products"] as List<Product>;
+            if(products == null)
+            {
+                products = new List<Product>();
+            }
+        }
+    }
+}
