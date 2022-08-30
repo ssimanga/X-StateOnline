c404f2d634a02cc38fbc5e0992ee0c1d5bef7438
Simanga Shabalala <simangae@gmail.com>
2022-08-30 08:11:09 +0200

    Initial Project Setup

---
+++ X-StateOnline.UI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="X_StateOnline.UI.MvcApplication" Language="C#" %>
---
+++ X-StateOnline.UI/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(X_StateOnline.UI.Startup))]
+namespace X_StateOnline.UI
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
