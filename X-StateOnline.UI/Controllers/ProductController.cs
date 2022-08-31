8ee94744d526e8c26b872a0ab6a481d3e16281d4
Simanga Shabalala <simangae@gmail.com>
2022-08-31 08:45:43 +0200

    Added the ProductController and Product Index View

---
+++ X-StateOnline.UI/Controllers/ProductController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using X_StateOnline.Core.Models;
+using X_StateOnline.DataAcess.Inmemory;
+
+namespace X_StateOnline.UI.Controllers
+{
+    public class ProductController : Controller
+    {
+        ProductRepository context;
+        public ProductController()
+        {
+            context = new ProductRepository();
+        }
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> products = context.Collection().ToList();
+            return View(products);
+        }
+    }
+}
