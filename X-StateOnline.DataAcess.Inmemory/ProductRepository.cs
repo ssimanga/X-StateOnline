c8abd9c3958c2800d1ff3a20a37d8eae472fa482
Simanga Shabalala <simangae@gmail.com>
2022-08-31 08:16:39 +0200

    Added end points methods

--- X-StateOnline.DataAcess.Inmemory/ProductRepository.cs
+++ X-StateOnline.DataAcess.Inmemory/ProductRepository.cs
@@ -21,5 +21,57 @@ namespace X_StateOnline.DataAcess.Inmemory
                 products = new List<Product>();
             }
         }
+
+        public void Commit()
+        {
+            cache["products"] = products;
+        }
+
+        public void Insert(Product p)
+        {
+            products.Add(p);
+        }
+
+        public void Update(Product product)
+        {
+            Product productToUpdate = products.Find(p => p.Id == product.Id);
+            if(productToUpdate != null)
+            {
+                productToUpdate = product;
+            }
+            else
+            {
+                throw new Exception("Product Was not Find");
+            }
+        }
+        public Product Find(string Id)
+        {
+            Product product = products.Find(p => p.Id == Id);
+            if (product != null)
+            {
+                return product;
+            }
+            else
+            {
+                throw new Exception("Product Was not Find");
+            }
+        }
+        public IQueryable<Product> Collection()
+        {
+            return products.AsQueryable();
+        }
+
+        public void Delete(string Id)
+        {
+            Product productToDelete = products.Find(p => p.Id == Id);
+            if (productToDelete != null)
+            {
+                products.Remove(productToDelete);
+            }
+            else
+            {
+                throw new Exception("Product Was not Find");
+            }
+        }
     }
 }
