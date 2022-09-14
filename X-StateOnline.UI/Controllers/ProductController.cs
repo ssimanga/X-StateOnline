26ae5101fce1b5c6c80e946ff605cf5b2c335939
Simanga Shabalala <simangae@gmail.com>
2022-09-14 08:47:50 +0200

    Images Upload

--- X-StateOnline.UI/Controllers/ProductController.cs
+++ X-StateOnline.UI/Controllers/ProductController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -33,7 +34,7 @@ namespace X_StateOnline.UI.Controllers
             return View(viewModel);
         }
         [HttpPost]
-        public ActionResult Create(Product product)
+        public ActionResult Create(Product product, HttpPostedFileBase file)
         {
             if (!ModelState.IsValid)
             {
@@ -41,6 +42,11 @@ namespace X_StateOnline.UI.Controllers
             }
             else
             {
+                if(file != null)
+                {
+                    product.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//" + product.Image));
+                }
                 context.Insert(product);
                 context.Commit();
                 return RedirectToAction("Index");
@@ -63,7 +69,7 @@ namespace X_StateOnline.UI.Controllers
             }
         }
         [HttpPost]
-        public ActionResult Edit(Product product, string Id)
+        public ActionResult Edit(Product product, string Id, HttpPostedFileBase file)
         {
             Product productToEdit = context.Find(Id);
             if (productToEdit == null)
@@ -76,9 +82,14 @@ namespace X_StateOnline.UI.Controllers
                 {
                     return View(product);
                 }
+                if(file != null)
+                {
+                    productToEdit.Image = product.Id + Path.GetExtension(file.FileName);
+                    file.SaveAs(Server.MapPath("//Content//ProductImages//" + productToEdit.Image));
+                }
                 productToEdit.Category = product.Category;
                 productToEdit.Description = product.Description;
-                productToEdit.Image = product.Image;
+             
                 productToEdit.Name = product.Name;
                 productToEdit.Price = product.Price;
                 context.Commit();
