94228fd9a796683bb60187813dcb4a953420af4c
Simanga Shabalala <simangae@gmail.com>
2022-09-15 08:38:50 +0200

    Product Listings Part 1

--- X-StateOnline.UI/Controllers/HomeController.cs
+++ X-StateOnline.UI/Controllers/HomeController.cs
@@ -3,14 +3,38 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using X_StateOnline.Core.Contracts;
+using X_StateOnline.Core.Models;
+using X_StateOnline.Core.ViewModels;
 
 namespace X_StateOnline.UI.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        IRepository<Product> context;
+        IRepository<ProductCategory> productCategories;
+        public HomeController(IRepository<Product> productContext, IRepository<ProductCategory> categoryContext)
         {
-            return View();
+            context = productContext;
+            productCategories = categoryContext;
+
+        }
+        public ActionResult Index(string Category = null)
+        {
+            List<Product> products;
+            List<ProductCategory> categories = productCategories.Collection().ToList();
+            if(Category == null)
+            {
+                products = context.Collection().ToList();
+            }
+            else
+            {
+                products = context.Collection().Where(p => p.Category == Category).ToList();
+            }
+            ProductListVM model = new ProductListVM();
+            model.Products = products;
+            model.ProductCategories = categories;
+            return View(model);
         }
 
         public ActionResult About()
